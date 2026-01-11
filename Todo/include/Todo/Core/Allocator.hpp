235f2b04bf67aa81bdaf2b0cf55579fe6171d894
Iannis Poquet <ianpo@live.fr>
2026-01-11 12:15:18 +0100

    Make LockFreeStack with double Counted Ref

--- Todo/include/Todo/Core/Allocator.hpp
+++ Todo/include/Todo/Core/Allocator.hpp
@@ -11,16 +11,21 @@ namespace Todo
     struct Allocator
     {
         template<typename T, typename ... Args>
-        T* Create(Args&& ... args)
+        T* create(Args&& ... args)
         {
             void* data = alloc(sizeof(T));
             TODO_ASSERT(data != nullptr, "cannot allocate data");
             std::construct_at<T>(data, std::forward<Args>(args)...);
             return static_cast<T*>(data);
         }
+        template<typename T, typename ... Args>
+        static T* Create(Args&& ... args);
 
         template<typename T>
-        void Delete(T* ptr)
+        static void Destroy(T* ptr);
+
+        template<typename T>
+        void destroy(T* ptr)
         {
             if (!ptr)
             {
@@ -37,6 +42,19 @@ namespace Todo
     };
 
     inline Allocator s_Allocator{};
+
+    template <typename T, typename ... Args>
+    T* Allocator::Create(Args&&... args)
+    {
+        return s_Allocator.create<T>(std::forward<Args>(args)...);
+    }
+
+    template <typename T>
+    void Allocator::Destroy(T* ptr)
+    {
+        s_Allocator.destroy(ptr);
+    }
+
 
 
 }
--- Todo/include/Todo/Multithreading/LockFreeStack.h
+++ Todo/include/Todo/Multithreading/LockFreeStack.h
@@ -3,97 +3,96 @@
 //
 
 #pragma once
+#include "Todo/Core/Allocator.hpp"
 
 namespace Todo
 {
-    template<std::copyable T>
+    template <std::copyable T>
     class LockFreeStack
     {
+        struct Node;
+
+        struct CountedNodePtr
+        {
+            int external_count;
+            Node* ptr;
+        };
+
         struct Node
         {
-            Node(const T& data) : data(std::make_shared<T>(data)) {}
+            Node(const T& data) : data(std::make_shared<T>(data)), internal_count{0}
+            {
+            }
+
             std::shared_ptr<T> data;
-            Node* next{nullptr};
+            std::atomic<int> internal_count;
+            CountedNodePtr next{nullptr};
         };
+
+    public:
+        ~LockFreeStack()
+        {
+            while (pop());
+        }
+
     public:
         void push(const T& data)
         {
-            Node* node = new Node(data);
-            node->next = head.load(std::memory_order_seq_cst);
-            while (!head.compare_exchange_weak(node->next, node, std::memory_order_seq_cst));
+            CountedNodePtr node_ptr;
+            node_ptr.ptr = Allocator::Create<Node>(data);
+            node_ptr.external_count = 1;
+            node_ptr.ptr->next = head.load(std::memory_order_relaxed);
+            while (!head.compare_exchange_weak(node_ptr.ptr->next, node_ptr, std::memory_order_release,
+                                               std::memory_order_relaxed));
         }
 
         std::shared_ptr<T> pop()
         {
-            thread_in_pop.fetch_add(1, std::memory_order_seq_cst);
-            Node* oldHead = head.load(std::memory_order_seq_cst);
-            while (oldHead && head.compare_exchange_weak(oldHead, oldHead->next, std::memory_order_seq_cst));
-            std::shared_ptr<T> result;
-            if (oldHead)
+            CountedNodePtr old_head = head.load(std::memory_order_relaxed);
+            for (;;)
             {
-                std::swap(result, oldHead->data);
-            }
-            try_reclaim(oldHead); // 'thread_in_pop' will be decreased here.
-            return result;
-        }
-    private:
-        static inline void delete_nodes(Node* nodes)
-        {
-            while (nodes)
-            {
-                Node* next = nodes->next;
-                delete nodes;
-                nodes = next;
-            }
-        }
-
-        void try_reclaim(Node* node)
-        {
-            if (thread_in_pop.load(std::memory_order_seq_cst) == 1)
-            {
-                Node* nodes_to_delete = to_be_deleted.exchange(nullptr, std::memory_order_seq_cst);
-                if (thread_in_pop.fetch_sub(1, std::memory_order_seq_cst) == 1)
+                increase_head_cunt(old_head);
+                Node* const ptr = old_head.ptr;
+                if (!ptr)
                 {
-                    delete_nodes(nodes_to_delete);
+                    return nullptr;
                 }
-                else
+
+                if (head.compare_exchange_strong(old_head, ptr->next, std::memory_order_release,
+                                                 std::memory_order_relaxed))
                 {
-                    chain_pending_nodes(nodes_to_delete);
+                    std::shared_ptr<T> res;
+                    res.swap(ptr->data);
+                    const int count_increase = old_head.external_count - 2;
+                    if (ptr->internal_count.fetch_add(count_increase, std::memory_order_release) == -count_increase)
+                    {
+                        Allocator::Destroy(ptr);
+                    }
+                    return res;
                 }
-                delete node;
+                else if (ptr->internal_count.fetch_add(1, std::memory_order_relaxed) == 1)
+                {
+                    ptr->internal_count.load(std::memory_order_acquire);
+                    Allocator::Destroy(ptr);
+                }
             }
-            else
-            {
-                chain_pending_nodes(node);
-                thread_in_pop.fetch_sub(1, std::memory_order_seq_cst);
-            }
-        }
-
-        void chain_pending_nodes(Node* nodes)
-        {
-            Node* last = nodes;
-            while (const Node* next = last->next)
-            {
-                last = next;
-            }
-            chain_pending_nodes(nodes, last);
-        }
-
-        void chain_pending_nodes(Node* first, Node* last)
-        {
-            last->next = to_be_deleted.load(std::memory_order_seq_cst);
-            while (!to_be_deleted.compare_exchange_weak(last->next, first));
-        }
-
-        void chain_pending_node(Node* node)
-        {
-            chain_pending_nodes(node, node);
         }
 
     private:
-        std::atomic<Node*> head;
-        std::atomic<uint32_t> thread_in_pop;
-        std::atomic<Node*> to_be_deleted;
+        void increase_head_cunt(CountedNodePtr& old_counter)
+        {
+            CountedNodePtr new_counter;
+            do
+            {
+                new_counter = old_counter;
+                ++new_counter.external_count;
+            }
+            while (!head.compare_exchange_strong(old_counter, new_counter, std::memory_order_acquire,
+                                                 std::memory_order_relaxed));
+            old_counter.external_count = new_counter.external_count;
+        }
 
+    private:
+        std::atomic<CountedNodePtr> head;
     };
 }
