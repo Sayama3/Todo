6147a7f351aab4aee91311243686c7094e3c0b43
Iannis Poquet <ianpo@live.fr>
2026-01-20 14:46:12 +0100

    Mini Test for the Promise & Futures

--- Examples/FinderApp/src/main.cpp
+++ Examples/FinderApp/src/main.cpp
@@ -7,11 +7,31 @@
 
 #include <iostream>
 
+#include "Todo/Async/Promise.hpp"
+
+
+using Duration = std::chrono::duration<uint64_t, std::nano>;
+using Clock = std::chrono::high_resolution_clock;
+using TimePoint = std::chrono::time_point<Clock, Duration>;
 
 int main(int argc, char* argv[])
 {
-	Todo::Initialize(Todo::GetOptimalThreadCount());
-	// Do things.
-	Todo::Destroy();
+	Todo::Promise<uint64_t> promise{};
+	// std::promise<uint64_t> promise{};
+	auto future = promise.get_future();
+	const TimePoint beg = Clock::now();
+	const bool ready = future.is_ready();
+	// const bool ready = future.wait_for(std::chrono::seconds{0}) != std::future_status::timeout;
+	const TimePoint end = Clock::now();
+
+	std::cout << "Time: " << (end - beg).count() << "ns" << std::endl;
+	if (ready)
+	{
+		std::cout << "is ready" << std::endl;
+	}
+	else
+	{
+		std::cout << "is not ready" << std::endl;
+	}
 	return 0;
 }
