f14e33360c2ab1c626c70f78788e24cdb2084be3
Iannis Poquet <ianpo@live.fr>
2026-01-21 15:58:33 +0100

    Re-test more the ThreadPool and the futures

--- Examples/FinderApp/src/main.cpp
+++ Examples/FinderApp/src/main.cpp
@@ -13,15 +13,24 @@ using Duration = std::chrono::duration<uint64_t, std::nano>;
 using Clock = std::chrono::high_resolution_clock;
 using TimePoint = std::chrono::time_point<Clock, Duration>;
 
+static inline int rand(int seed)
+{
+	constexpr int a = 1103515245;
+	constexpr int c = 12345;
+	constexpr int m = 2147483648;//2e31;
+	seed = (a * seed + c) % m;
+	return seed;
+}
+
 int main(int argc, char* argv[])
 {
 	{
-		Todo::Promise<uint64_t> promise{};
-		// std::promise<uint64_t> promise{};
+		// Todo::Promise<uint64_t> promise{};
+		std::promise<uint64_t> promise{};
 		auto future = promise.get_future();
 		const TimePoint beg = Clock::now();
-		const bool ready = future.is_ready();
-		// const bool ready = future.wait_for(std::chrono::seconds{0}) != std::future_status::timeout;
+		// const bool ready = future.is_ready();
+		const bool ready = future.wait_for(std::chrono::seconds{0}) != std::future_status::timeout;
 		const TimePoint end = Clock::now();
 
 		std::cout << "Time: " << (end - beg).count() << "ns" << std::endl;
@@ -38,13 +47,26 @@ int main(int argc, char* argv[])
 	{
 		Todo::ThreadPool thread_pool;
 
-		auto f = thread_pool.Submit([](){std::this_thread::sleep_for(std::chrono::seconds(5)); return 1;});
+		std::array<Todo::Future<int>, 6> futures;
+		futures[1-1] = thread_pool.Submit([](){auto id = std::this_thread::get_id(); std::this_thread::sleep_for(std::chrono::seconds(rand(*(unsigned int*)&id) % 5u));return 1;});
+		futures[2-1] = thread_pool.Submit([](){auto id = std::this_thread::get_id(); std::this_thread::sleep_for(std::chrono::seconds(rand(*(unsigned int*)&id) % 5u));return 2;});
+		futures[3-1] = thread_pool.Submit([](){auto id = std::this_thread::get_id(); std::this_thread::sleep_for(std::chrono::seconds(rand(*(unsigned int*)&id) % 5u));return 3;});
+		futures[4-1] = thread_pool.Submit([](){auto id = std::this_thread::get_id(); std::this_thread::sleep_for(std::chrono::seconds(rand(*(unsigned int*)&id) % 5u));return 4;});
+		futures[5-1] = thread_pool.Submit([](){auto id = std::this_thread::get_id(); std::this_thread::sleep_for(std::chrono::seconds(rand(*(unsigned int*)&id) % 5u));return 5;});
+		futures[6-1] = thread_pool.Submit([](){auto id = std::this_thread::get_id(); std::this_thread::sleep_for(std::chrono::seconds(rand(*(unsigned int*)&id) % 5u));return 6;});
 
-		while (!f.is_ready())
+		uint64_t done = 6;
+		while (done)
 		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			for (auto& future : futures)
+			{
+				if (future.is_ready() && future.is_valid())
+				{
+					--done;
+					std::cout << "Future " << future.get() << " is done" << std::endl;
+				}
+			}
 		}
-		std::cout << "Future is: " << f.get() << std::endl;
 	}
 
 	return 0;
